7bba0c47b618d5b9bb02c4f82b8c676e3f3c80d8
Reagan Barrington <reagan@barringtonfamily.net>
2020-11-04 13:27:17 -0500

    first dynamic data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  // has an array which includes Recipe objects, as defined in our model.
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simple a test.', 'https://www.forksoverknives.com/wp-content/uploads/fly-images/66822/Tex-Mex-Pita-Pizzas_WP-688x387-c.jpg')
+  ];
 
   constructor() { }
 
